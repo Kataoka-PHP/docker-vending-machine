02a4f66c9d4c713cd555b300649965b7e5682153
Kataoka-PHP <kataoka.soma9@gmail.com>
2022-02-14 00:07:34 +0900

    [create]売上ログテーブルを作成
    テーブル定義書の仕様を満たすため

---
+++ src/database/migrations/2022_02_13_040508_create_sales_log_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSalesLogTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sales_log', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('product_id');
+            $table->foreign('product_id')->references('id')->on('product_masters')->onDelete('cascade');
+            $table->integer('purchase_price');
+            $table->dateTime('purchase_time');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sales_log');
+    }
+}
