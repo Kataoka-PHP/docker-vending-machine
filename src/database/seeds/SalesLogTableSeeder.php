cb308d7edbb9bb8d4883a17477ee50f17dae39c3
Kataoka-PHP <kataoka.soma9@gmail.com>
2022-02-23 13:30:12 +0900

    [create]新規シーダーファイルを作成
    売上ログテーブルのシーダーファイルを作成

---
+++ src/database/seeds/SalesLogTableSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class SalesLogTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $param = [
+            'product_id' => 1,
+            'purchase_price' => 200,
+        ];
+        DB::table('sales_log')->insert($param);
+        $param = [
+            'product_id' => 2,
+            'purchase_price' => 420,
+        ];
+        DB::table('sales_log')->insert($param);
+        $param = [
+            'product_id' => 3,
+            'purchase_price' => 3800,
+        ];
+        DB::table('sales_log')->insert($param);
+    }
+}
