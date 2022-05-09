8628c5188448811343be12f80342cab987c98dd2
Kataoka-PHP <kataoka.soma9@gmail.com>
2022-05-10 07:17:17 +0900

    [update]テストデータの数を変更
    テストデータを３個から１２個へ変更

--- src/database/seeds/ProductMastersTableSeeder.php
+++ src/database/seeds/ProductMastersTableSeeder.php
@@ -33,5 +33,68 @@ class ProductMastersTableSeeder extends Seeder
             'display_order' => 3,
         ];
         DB::table('product_masters')->insert($param);
+        $param = [
+            'product_name' => 'コーラ',
+            'image' => base64_encode(Storage::get('cola.jpg')),
+            'price' => 100,
+            'display_order' => 1,
+        ];
+        DB::table('product_masters')->insert($param);
+        $param = [
+            'product_name' => 'ビール',
+            'image' => base64_encode(Storage::get('beer.jpg')),
+            'price' => 210,
+            'display_order' => 2,
+        ];
+        DB::table('product_masters')->insert($param);
+        $param = [
+            'product_name' => '食パン',
+            'image' => base64_encode(Storage::get('plain_blead.jpg')),
+            'price' => 1900,
+            'display_order' => 3,
+        ];
+        DB::table('product_masters')->insert($param);
+        $param = [
+            'product_name' => 'コーラ',
+            'image' => base64_encode(Storage::get('cola.jpg')),
+            'price' => 100,
+            'display_order' => 1,
+        ];
+        DB::table('product_masters')->insert($param);
+        $param = [
+            'product_name' => 'ビール',
+            'image' => base64_encode(Storage::get('beer.jpg')),
+            'price' => 210,
+            'display_order' => 2,
+        ];
+        DB::table('product_masters')->insert($param);
+        $param = [
+            'product_name' => '食パン',
+            'image' => base64_encode(Storage::get('plain_blead.jpg')),
+            'price' => 1900,
+            'display_order' => 3,
+        ];
+        DB::table('product_masters')->insert($param);
+        $param = [
+            'product_name' => 'コーラ',
+            'image' => base64_encode(Storage::get('cola.jpg')),
+            'price' => 100,
+            'display_order' => 1,
+        ];
+        DB::table('product_masters')->insert($param);
+        $param = [
+            'product_name' => 'ビール',
+            'image' => base64_encode(Storage::get('beer.jpg')),
+            'price' => 210,
+            'display_order' => 2,
+        ];
+        DB::table('product_masters')->insert($param);
+        $param = [
+            'product_name' => '食パン',
+            'image' => base64_encode(Storage::get('plain_blead.jpg')),
+            'price' => 1900,
+            'display_order' => 3,
+        ];
+        DB::table('product_masters')->insert($param);
     }
 }
