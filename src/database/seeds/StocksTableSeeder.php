030235c95f80b69cc8fe2d6f7c77663fbfe9a3ab
Kataoka-PHP <kataoka.soma9@gmail.com>
2022-05-10 07:18:53 +0900

    [update]テストデータの数を変更
    テストデータを３個から１２個へ変更

--- src/database/seeds/StocksTableSeeder.php
+++ src/database/seeds/StocksTableSeeder.php
@@ -18,11 +18,56 @@ class StocksTableSeeder extends Seeder
         DB::table('stocks')->insert($param);
         $param = [
             'product_master_id' => 2,
-            'stock' => 3,
+            'stock' => 40,
         ];
         DB::table('stocks')->insert($param);
         $param = [
             'product_master_id' => 3,
+            'stock' => 4,
+        ];
+        DB::table('stocks')->insert($param);
+        $param = [
+            'product_master_id' => 4,
+            'stock' => 2,
+        ];
+        DB::table('stocks')->insert($param);
+        $param = [
+            'product_master_id' => 5,
+            'stock' => 3,
+        ];
+        DB::table('stocks')->insert($param);
+        $param = [
+            'product_master_id' => 6,
+            'stock' => 4,
+        ];
+        DB::table('stocks')->insert($param);
+        $param = [
+            'product_master_id' => 7,
+            'stock' => 2,
+        ];
+        DB::table('stocks')->insert($param);
+        $param = [
+            'product_master_id' => 8,
+            'stock' => 3,
+        ];
+        DB::table('stocks')->insert($param);
+        $param = [
+            'product_master_id' => 9,
+            'stock' => 4,
+        ];
+        DB::table('stocks')->insert($param);
+        $param = [
+            'product_master_id' => 10,
+            'stock' => 2,
+        ];
+        DB::table('stocks')->insert($param);
+        $param = [
+            'product_master_id' => 11,
+            'stock' => 3,
+        ];
+        DB::table('stocks')->insert($param);
+        $param = [
+            'product_master_id' => 12,
             'stock' => 4,
         ];
         DB::table('stocks')->insert($param);
