ca30692ad94d40d419b7754ef604499afd8cad8a
Kataoka-PHP <kataoka.soma9@gmail.com>
2022-08-21 09:28:50 +0900

    [create]共通レイアウトのヘッダーを作成

---
+++ src/resources/views/layouts/common.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
+    <title>Vending-Machine</title>
+</head>
+<body>
+    @include('parts.header')
+    @yield('content')
+</body>
+</html>
+
